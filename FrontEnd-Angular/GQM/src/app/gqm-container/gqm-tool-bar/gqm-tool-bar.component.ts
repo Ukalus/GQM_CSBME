5aea23e80576865efb30e441d2fe107dcbe8c035
lulmann26 <nicolas.senftleben@csbme.de>
2024-09-07 21:35:23 +0200

    frontend-update

--- FrontEnd-Angular/GQM/src/app/gqm-container/gqm-tool-bar/gqm-tool-bar.component.ts
+++ FrontEnd-Angular/GQM/src/app/gqm-container/gqm-tool-bar/gqm-tool-bar.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {ViewEncapsulation } from '@angular/core';
+import {MatDialog, MatDialogModule, MatDialogActions,MatDialogClose,MatDialogContent,MatDialogTitle,} from '@angular/material/dialog';
+import { Y } from '@angular/cdk/keycodes';
 
 
 @Component({
@@ -16,11 +18,57 @@ import {ViewEncapsulation } from '@angular/core';
     FormsModule,
     MatInputModule,
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDialogModule
   ],
   templateUrl: './gqm-tool-bar.component.html',
   styleUrl: './gqm-tool-bar.component.scss'
 })
 export class GqmToolBarComponent {
-   GqmList:Array<any> = ["eins","kein"]
+  readonly dialog = inject(MatDialog);
+  GqmList:Array<any> = ["eins","kein"]
+
+
+  AddDialog(section:number):void{
+    console.log(section)
+   }
+  DeletDialog(section:number):void{
+    const dialogRef = this.dialog.open(deletDialog, {
+      //data: {name: this.type()},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+    });
+   }
+
+}
+
+
+@Component({
+  encapsulation: ViewEncapsulation.None,
+  selector: 'app-DeletDialog',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatDialogModule
+  ],
+  templateUrl: './deletdialog.html',
+  styleUrl: './deletdialog.html'
+})
+export class deletDialog{
+  
+   title:string = "Kein Tittle gesetzt"
+    type:number = 0; //soll angeben in welcher spalte der Tabelle 
+
+  ngOnInit(){
+    console.log("test ob der Dialog auch iniziealisiert wird")
+  }
+}
+export interface DeletDialogData{
+  type:number
 }
