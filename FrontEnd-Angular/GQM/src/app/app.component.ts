b2dba9129b0ee8c29792091e2b4f853474736887
lulmann26 <nicolas.senftleben@csbme.de>
2024-09-07 22:22:36 +0200

    login-Page und so pipapo müsst einfach in aa.comonent.html auskommentieren ihr checkt schon

--- FrontEnd-Angular/GQM/src/app/app.component.ts
+++ FrontEnd-Angular/GQM/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component, NgModule, OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GQMContainerComponent } from './gqm-container/gqm-container.component';
+import { LoginServiceTs } from './login-service.ts';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    GQMContainerComponent
+    GQMContainerComponent,
+    LoginPageComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
